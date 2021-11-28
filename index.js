c57adca80c03f3082bf4256a72115c5c3f65844a
Ibrohim <ismailovibrokhim98@gmail.com>
2021-11-28 23:47:43 +0500

    yangi funksiyalar qo'shildi

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+        const audio = new Audio();
+        audio.src = "./mp3/zikr.mp3"
 
+        const audio1 = new Audio();
+        audio1.src = "./mp3/alhamdulillah.mp3"
+
+        const audio2 = new Audio();
+        audio2.src = "./mp3/allohuakbar.mp3"
 
         function vibrate (ms){
             navigator.vibrate(ms)
         }
 
         
-
-
         document.querySelector('.count').addEventListener('click', function(){
             var number = document.querySelector('.box-number');
             console.log( typeof parseInt( number.value ) );
@@ -17,17 +22,20 @@
                 navigator.vibrate([100])
                 // document.querySelector('.box-number').value = 0;
                 document.querySelector('#zikr').innerText = "Subhanalloh";
+                document.querySelector('#play').audio = audio;
                 console.log("33");
                 
             }else if(parseInt( number.value ) === 33 ){
                 navigator.vibrate([700])
                 // document.querySelector('.box-number').value = 0;
                 document.querySelector('#zikr').innerText = "Alhamdulillah";
+                document.querySelector('#play').audio = audio1;
                 console.log("33");
             }
             else if(parseInt( number.value ) === 66){
                 navigator.vibrate([700])
                 document.querySelector('#zikr').innerText = "Allahu Akbar";
+                document.querySelector('#play').audio = audio2;
                 console.log("66");
             }
             else if(parseInt( number.value ) === 99){
@@ -37,6 +45,7 @@
             }
             
         });
+
 
         document.querySelector('.reset').addEventListener('click', function(){
             navigator.vibrate([100])
